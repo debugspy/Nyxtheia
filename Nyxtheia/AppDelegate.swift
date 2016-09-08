2d8e734a404b1d7f63b5fe0b02da328f4b316033
Jacob Gaffney <jacobgaffney@me.com>
2016-09-08 17:51:22 +1000

    Initial Commit

---
+++ Nyxtheia/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Nyxtheia
+//
+//  Created by jacques on 8/09/2016.
+//  Copyright © 2016 Jacob Gaffney. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
